023af46ef19c165fd25b675e019a19e06213d698
g-bispo <gbispo@usp.br>
2025-09-13 13:29:27 -0300

    Teste para o Petsc

---
+++ assignment03/gbispo/petsctest.c
@@ -0,0 +1,27 @@
+#include <petsc.h>
+
+int main (int argc, char **argv) {
+    PetscErrorCode  ierr;
+    PetscMPIInt     rank;
+    PetscInt        i;
+    PetscReal       localval, globalsum;
+
+    PetscInitialize(&argc,&argv,NULL,"Compute e in parallel with PETSc.\n\n");
+    ierr = MPI_Comm_rank(PETSC_COMM_WORLD,&rank); CHKERRQ(ierr);
+
+    //Compute 1/n! where n = (rank of processes +1)
+    localval = 1.0;
+    for (i = 2; i< rank+1; i++)
+        localval /= i;
+
+    //Sum the contributions over all processes
+    ierr = MPI_Allreduce(&localval, &globalsum, 1, MPIU_REAL, MPIU_SUM,
+                            PETSC_COMM_WORLD); CHKERRQ(ierr);
+
+    //output estimate of e and report on work from each process
+    ierr = PetscPrintf (PETSC_COMM_WORLD,"e is about %17.15f \n" ,globalsum) ; CHKERRQ(ierr);
+    ierr = PetscPrintf (PETSC_COMM_SELF,
+    "rank %d did %d flops \n" ,rank , ( rank > 0) ? rank-1 : 0) ;
+    CHKERRQ( ierr ) ;
+    return PetscFinalize () ;
+}
